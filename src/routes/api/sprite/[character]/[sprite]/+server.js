527aff1c7bf769ce79665fe63f9381b408c673b0
lingon <48642331+lingonsylt@users.noreply.github.com>
2026-03-16 14:52:51 +0100

    remove catch

--- src/routes/api/sprite/[character]/[sprite]/+server.js
+++ src/routes/api/sprite/[character]/[sprite]/+server.js
@@ -3,22 +3,17 @@ import fs from 'fs';
 import path from 'path';
 
 export async function GET({ params }) {
-	try {
-		const filePath = path.resolve(`src/lib/sprites/${params.character}/${params.sprite}.png`);
+	const filePath = path.resolve(`src/lib/sprites/${params.character}/${params.sprite}.png`);
 
-		if (!fs.existsSync(filePath)) {
-			error(404, 'Sprite does not exist');
-		}
-
-		const file = fs.readFileSync(filePath);
-
-		return new Response(file, {
-			headers: {
-				'Content-Type': 'image/png'
-			}
-		});
-	} catch (err) {
-		console.error(err);
-		error(500, 'Server error');
+	if (!fs.existsSync(filePath)) {
+		error(404, 'Sprite does not exist');
 	}
+
+	const file = fs.readFileSync(filePath);
+
+	return new Response(file, {
+		headers: {
+			'Content-Type': 'image/png'
+		}
+	});
 }
