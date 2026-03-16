89a776aa8a1eac6d4d533cb038e6c14487a3c97b
lingon <48642331+lingonsylt@users.noreply.github.com>
2026-03-16 09:38:05 +0100

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,14 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-	import { onMount } from 'svelte';
-	import { gameTick } from '$lib/game.svelte';
 
 	let { children } = $props();
-
-	let keys = [];
-	let keyEvents = {};
-
-	function handleKeyDown(e) {
-		keys.push(e.key);
-	}
-	function handleKeyUp(e) {
-		keys.splice(keys.findIndex(e.key));
-	}
-
-	onMount(() => {
-		setInterval(gameTick, 100);
-	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-¨
-<svelte:window onkeydown={handleKeyDown} onkeyup={handleKeyUp} />
 
-<div class="container">
-	<main>
-		{@render children()}
-	</main>
-</div>
+{@render children()}
 
 <style>
-	.container {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100vw;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	:global(body) {
-		background-color: black;
-	}
 </style>
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import { resolve } from '$app/paths';
-</script>
-
-<a href={resolve('/start')} class="play">Play</a>
